b6abc1f372c8d13915638bfdbdc443fd8ac8ed36
Ahmedfayed <ahmedfayed17@gmail.com>
2023-05-26 17:05:44 +0200

    my unfinished watch list

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,51 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: %i[show update edit destroy]
+
+  def index
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def show
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to @list, notice: 'you have succesfully created a List Hurrrrrrrrrrrra!!!!!'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @list
+  end
+
+  def update
+    if @list.update(list_params)
+      redirect_to @list, notice: 'list was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_url, notice: 'list was successfully destroyed.'
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "cleaning db.."
+
+Movie.destroy_all
+
+puts "populating db.."
+url = "https://tmdb.lewagon.com/movie/top_rated"
+movie_serialized = URI.open(url).read
+movies = JSON.parse(movie_serialized)
+movies["results"].each do |movie|
+  # puts "#{movie["title"]} - #{movie["overview"]} #{movie["vote_average"]} #{movie["poster_path"]}"
+  Movie.create(title: movie["title"], overview: movie["overview"], rating: movie["vote_average"], poster_url: "https://image.tmdb.org/t/p/w500/#{movie["poster_path"]}")
+end
